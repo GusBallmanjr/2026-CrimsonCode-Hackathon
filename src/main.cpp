6d18568153cd70638c8da2844030e5188b79b38b
MrThomas <jawisco@icloud.com>
2026-02-21 21:26:51 -0800

    added --help command

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,10 @@ int main(int argc, char* argv[]) {
   std::string flag1 = argv[1];
   std::string file1 = argv[2];
   std::string file2 = argv[3];
-
-  if (flag1 == "-create-keys") {
+  if (flag1 == "--help") {
+    std::cout << "joshua | public / private key encryption" << std::endl << "--help | prints this list" << std::endl << "-create-keys <name> | creates a set of public / private keys" << std::endl << "-encrypt <decrypted-file> <public-key> | encrypts file with public key" << std::endl << "-decrypt <encrypted-file> <private-key> | decrypts file with private key" << std::endl;
+  }
+  else if (flag1 == "-create-keys") {
     if (argc == 2) createKeys(file1);
     else std::cout << "\"-create-keys\" requires 1 argument" << std::endl;
   } else if (flag1 == "-encrypt") {
