601c00578fb6d4999d5c5729286f0847c4d67a88
Mr Thomas <96408080+mrthomaslibrary@users.noreply.github.com>
2026-02-23 17:08:09 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]) {
   std::string file1 = argv[2];
   std::string file2 = argv[3];
   if (flag1 == "--help") {
-    std::cout << "joshua | public / private key encryption" << std::endl << "--help | prints this list" << std::endl << "-create-keys <name> | creates a set of public / private keys" << std::endl << "-encrypt <decrypted-file> <public-key> | encrypts file with public key" << std::endl << "-decrypt <encrypted-file> <private-key> | decrypts file with private key" << std::endl;
+    std::cout << "ogre-enc | public / private key encryption" << std::endl << "--help | prints this list" << std::endl << "-create-keys <name> | creates a set of public / private keys" << std::endl << "-encrypt <decrypted-file> <public-key> | encrypts file with public key" << std::endl << "-decrypt <encrypted-file> <private-key> | decrypts file with private key" << std::endl;
   }
   else if (flag1 == "-create-keys") {
     if (argc == 2) createKeys(file1);
@@ -30,7 +30,7 @@ int main(int argc, char* argv[]) {
       std::cout << "\"-encrypt\" requires two arguments" << std::endl;
     }
   } else {
-    std::cout << "joshua requires proper arguments. See --help for more info" << std::endl;
+    std::cout << "ogre requires proper arguments. See --help for more info" << std::endl;
   }
 
 }
